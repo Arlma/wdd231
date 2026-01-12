4d4bebdd9f0b0af73915b5e5dafda6d62cead459
Arlma <akumumaria56@gmail.com>
2026-01-12 15:38:10 +0300

    W04

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,8 +1,10 @@
+// Wait until the page is fully loaded
 document.addEventListener("DOMContentLoaded", () => {
+  // Get the hidden timestamp input
   const timestampField = document.getElementById("timestamp");
+  
+  // Fill it with current date & time
   if (timestampField) {
     timestampField.value = new Date().toISOString();
   }
 });
-
-
